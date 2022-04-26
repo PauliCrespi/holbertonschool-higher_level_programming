69eb08a924e03caa6efbafb4dfdc7feda529d1b0
PauliCrespi <4377@holbertonschool.com>
2022-04-26 09:26:43 -0700

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0,10):
+    for n in range(i, 10):
+        if n != i:
+            if i != 8 and i != 9:
+                print(f"{i}{n}, ", end='')
+print(f"89")
