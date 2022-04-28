beaa5fddd4e87fe0317e8f4a9ed64c50529212a6
PauliCrespi <4377@holbertonschool.com>
2022-04-28 08:22:02 -0700

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,16 +7,16 @@ if __name__ == "__main__":
         exit(1)
     if (sys.argv[2] == '+'):
         print(f"{sys.argv[1]} + {sys.argv[3]} = \
- {add(int(sys.argv[1]), int(sys.argv[3]))}")
+{add(int(sys.argv[1]), int(sys.argv[3]))}")
     elif (sys.argv[2] == '-'):
         print(f"{sys.argv[1]} - {sys.argv[3]} = \
- {sub(int(sys.argv[1]), int(sys.argv[3]))}")
+{sub(int(sys.argv[1]), int(sys.argv[3]))}")
     elif (sys.argv[2] == '*'):
         print(f"{sys.argv[1]} * {sys.argv[3]} = \
- {mul(int(sys.argv[1]), int(sys.argv[3]))}")
+{mul(int(sys.argv[1]), int(sys.argv[3]))}")
     elif (sys.argv[2] == '/'):
         print(f"{sys.argv[1]} / {sys.argv[3]} = \
- {div(int(sys.argv[1]), int(sys.argv[3]))}")
+{div(int(sys.argv[1]), int(sys.argv[3]))}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
