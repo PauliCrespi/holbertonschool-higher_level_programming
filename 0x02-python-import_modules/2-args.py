a12ec4ba3603bc6ebfa06f714feb5dd11e5d66e9
PauliCrespi <4377@holbertonschool.com>
2022-04-28 06:46:07 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if (len(sys.argv) == 1):
+    print(f"0 arguments.")
+elif (len(sys.argv) == 2):
+    print(f"{len(sys.argv) - 1} argument:\n1: {sys.argv[1]}")
+else:
+    print(f"{len(sys.argv) - 1} arguments:")
+    for i in range(1, len(sys.argv)):
+        print(f"{i}: {sys.argv[i]}")
