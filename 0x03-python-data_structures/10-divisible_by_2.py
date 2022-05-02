b79fb3bfafd4567e501279353ee924656f8b5b26
PauliCrespi <4377@holbertonschool.com>
2022-05-02 12:40:25 -0700

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = my_list.copy()
+    if my_list:
+        for i in range(len(my_list)):
+            if my_list[i] % 2 == 0:
+                new[i] = True
+            else:
+                new[i] = False
+        return new
