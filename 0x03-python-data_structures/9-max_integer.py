27fad724d782ed9635ec8e229d9c34ce7e211ba2
PauliCrespi <4377@holbertonschool.com>
2022-05-02 12:31:58 -0700

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,7 @@
 def max_integer(my_list=[]):
     maxi = 0
     if my_list:
+        maxi = my_list[0]
         for i in range(len(my_list)):
             if my_list[i] > maxi:
                 maxi = my_list[i]
