e989241a0b5d7e3ac881a1ca3d647b2137e21e16
PauliCrespi <4377@holbertonschool.com>
2022-05-02 10:52:29 -0700

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    lem1 = len(matrix)
+    for i in range(lem1):
+        for j in range(len(matrix[i])):
+            if j < (len(matrix[i]) - 1):
+                print(f"{matrix[i][j]}", end=' ')
+            else:
+                print(f"{matrix[i][j]}", end='')
+        print("$")
