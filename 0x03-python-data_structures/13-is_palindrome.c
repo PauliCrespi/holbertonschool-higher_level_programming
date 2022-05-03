c4c0b02ed98997f04abbc1746765c588b5fdad9a
PauliCrespi <4377@holbertonschool.com>
2022-05-02 19:25:50 -0700

    lists palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,34 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
+/**
+ * is_palindrome - main
+ * @head : list
+ * Return: 1 if palindrome or 0
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_t **last = head;
 	int i = 0, j = 0;
 	int flag = 0;
+
 	if (head == NULL)
 	{
 		return (1);
 	}
-	while((*last)->next != NULL)
+	while ((*last)->next != NULL)
 	{
 		i = i + 1;
 		*last = (*last)->next;
 	}
 	i = i - 1;
-	while(head != NULL)
+	while (head != NULL)
 	{
 		if (head == last)
 		{
 			*head = (*head)->next;
 			last = head;
-			for(j = 0; j <= i; j++)
+			for (j = 0; j <= i; j++)
 			{
 				*last = (*last)->next;
 			}
