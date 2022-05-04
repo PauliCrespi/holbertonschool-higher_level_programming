f115e87dccbcd07268c51ffa64e7502fa3aedde2
PauliCrespi <4377@holbertonschool.com>
2022-05-04 12:31:17 -0700

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    n = [d[i] for i in roman_string.upper() if i in d]
+    r = sum([i if i >= n[min(j + 1, len(n) - 1)]
+            else -i for j, i in enumerate(n)])
+    return r
