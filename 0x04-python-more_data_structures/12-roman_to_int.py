990d7aa6efafd26744d38e009b56651cbbb7d9f5
PauliCrespi <4377@holbertonschool.com>
2022-05-04 12:33:58 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string and type(roman_string) is not str:
+    if not roman_string or type(roman_string) is not str:
         return 0
     d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     n = [d[i] for i in roman_string.upper() if i in d]
