f517816262081e4e841c5f8c03138f96c7bdabf1
PauliCrespi <4377@holbertonschool.com>
2022-05-16 08:47:42 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     c = 0
     try:
         for c in range(x):
-            print(f"{my_list[c]}", end = "")
+            print(f"{my_list[c]}", end="")
         print("")
-    except:
+    except ValueError:
         print("")
         return c
     return(x)
