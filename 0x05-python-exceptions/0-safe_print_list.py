59ccaf0d6d328e803b9ca6aad456529cb24a0178
PauliCrespi <4377@holbertonschool.com>
2022-05-16 08:40:49 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    try:
+        for c in range(x):
+            print(f"{my_list[c]}", end = "")
+        print("")
+    except:
+        print("")
+        return c
+    return(x)
