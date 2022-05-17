f989cc9a1344d55aebc66252e5256349cf643f9c
PauliCrespi <4377@holberton.com>
2022-05-17 12:31:54 -0700

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""task 3"""
+
+
 class Square:
+    """class square"""
     def __init__(self, size=0):
+        """size init"""
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
+        """area func"""
         a = self.__size * self.__size
-        return a	
+        return a
