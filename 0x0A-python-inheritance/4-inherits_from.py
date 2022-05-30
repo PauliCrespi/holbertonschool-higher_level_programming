6df8731dca1d732ed2ca8b4720936171f58dc68a
PauliCrespi <4377@holberton.com>
2022-05-30 16:05:49 -0700

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """true or false"""
 
-    if type(obj) !=  a_class and issubclass(type(obj), a_class):
+    if (type(obj) is not a_class and issubclass(type(obj), a_class)):
         return True
     else:
         return False
