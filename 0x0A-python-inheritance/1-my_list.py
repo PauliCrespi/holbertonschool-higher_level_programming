8fe0fbbc3965ffc7ca733c291622a1829b5da861
PauliCrespi <4377@holberton.com>
2022-05-30 14:31:41 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""task 1"""
+
+
 class MyList(list):
     """MyList class"""
-
-
     def print_sorted(self):
         """print sorted list function"""
         print(sorted(self))
