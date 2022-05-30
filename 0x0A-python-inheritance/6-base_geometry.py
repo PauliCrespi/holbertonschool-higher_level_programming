08cdf57db9e5794b05839a85c12867bd538bd247
PauliCrespi <4377@holberton.com>
2022-05-30 16:13:36 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""task 6"""
+
+
+class BaseGeometry:
+    """Class"""
+    pass
+
+    def area(self):
+        """area func"""
+        raise Exception("area() is not implemented")
