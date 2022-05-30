0f0c03fe52e25e49a6fc862fc0f6b0f92d37bed3
PauliCrespi <4377@holberton.com>
2022-05-30 16:04:33 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""task 4"""
+
+
+def inherits_from(obj, a_class):
+    """true or false"""
+
+    if type(obj) !=  a_class and issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
