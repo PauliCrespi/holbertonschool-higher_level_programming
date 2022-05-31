fe0c7655a4314afb40e7504dd317aa1c9408f82e
PauliCrespi <4377@holberton.com>
2022-05-31 09:55:25 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""task 9"""
+
+
+class Student:
+    """class student"""
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json func"""
+        if type(attrs) == list:
+            for element in attrs:
+                if type(element) != str:
+                    break
+            new = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    new[i] = self.__dict__[i]
+            return(new)
+        return (self.__dict__)
