2003c4fc21b913c4a8011881382d56f3ee814eb0
PauliCrespi <4377@holberton.com>
2022-06-01 08:56:20 -0700

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,6 +4,14 @@
 
 def pascal_triangle(n):
     """pascale"""
-    new = [[]]
+    new = []
+    lst = []
     if n <= 0:
         return new
+    for x in range(n):
+        new = [1]
+        if x > 0:
+            for i in range(x):
+                new.append(sum(lst[-1][i:i+2]))
+        lst.append(new)
+    return (lst)
