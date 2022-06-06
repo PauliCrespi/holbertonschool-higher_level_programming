87c680c977cd504cb4abc8ff48b898df6c87b5c1
PauliCrespi <4377@holberton.com>
2022-06-06 09:19:07 -0700

    rec

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,13 +20,13 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """setter"""
-        if (type(width) is not int):
+        if (type(value) is not int):
             raise TypeError("width must be an integer")
-        if width <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -34,13 +34,13 @@ class Rectangle(Base):
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """setter"""
-        if (type(height) is not int):
+        if (type(value) is not int):
             raise TypeError("height must be an integer")
-        if height <= 0:
+        if value <= 0:
             raise ValueError("height muste be > 0")
-        self.__height = height
+        self.__height = value
 
     @property
     def x(self):
@@ -48,25 +48,25 @@ class Rectangle(Base):
         return self.__x
 
     @x.setter
-    def x(self, x):
+    def x(self, value):
         """setter"""
-        if (type(x) is not int):
+        if (type(value) is not int):
             raise TypeError("x must be an integer")
-        if x < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = x
+        self.__x = value
 
     @property
     def y(self):
         return self.__y
 
     @y.setter
-    def y(self, y):
-        if (type(y) is not int):
+    def y(self, value):
+        if (type(value) is not int):
             raise TypeError("y must be an integer")
-        if y < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = y
+        self.__y = value
 
     def area(self):
         """area func"""
