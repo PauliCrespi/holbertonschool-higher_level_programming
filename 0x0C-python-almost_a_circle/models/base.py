6ba911c8174894de068c1f8d49be85ac55c473ba
PauliCrespi <4377@holberton.com>
2022-06-06 09:47:41 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,7 +38,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """from json to string"""
-        if json_string is None or len(json_string) == 0:
+        if json_string is None or json_string == "":
             return "[]"
         return(json.loads(json_string))
 
