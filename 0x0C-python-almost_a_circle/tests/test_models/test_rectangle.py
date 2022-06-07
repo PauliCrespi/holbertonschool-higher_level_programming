7e819a57ac0d756fec4222f45b6dbdbf7662f25e
PauliCrespi <4377@holberton.com>
2022-06-06 18:43:52 -0700

    test rec

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Unittest"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """test base"""
+
+
+    def test_user_activation(self):
+        pass
+
+    def test_user_points_update(self):
+        pass
+
+    def test_user_level_change(self):
+        pass
+
+    def test_0(self):
+        """0"""
+        a = Rectangle(6, 7)
+        self.assertEqual(a.width, 6)
+        self.assertEqual(a.height, 7)
+
+    def test_1(self):
+        """1"""
+        b = Rectangle(61111, 67967, 8)
+        self.assertEqual(b.width, 61111)
+        self.assertEqual(b.height, 67967)
+        self.assertEqual(b.x, 8)
+
+    def test_2(self):
+        """2"""
+        c = Rectangle(61111, 67967, 8, 90)
+        self.assertEqual(c.width, 61111)
+        self.assertEqual(c.height, 67967)
+        self.assertEqual(c.x, 8)
+        self.assertEqual(c.y, 90)
