6f4fa82b6bf344f27dcbecb286d4c03dbe42b02e
PauliCrespi <4377@holberton.com>
2022-08-01 10:50:21 -0700

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+const num = parseInt(args[2]);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < num; i++) {
+  console.log('C is fun');
+}
