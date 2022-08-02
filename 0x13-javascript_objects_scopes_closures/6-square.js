9eb99a43007b3ec7cbe9aa2d32c156136f5b0185
PauliCrespi <4377@holberton.com>
2022-08-02 09:11:03 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square') {
+module.exports = class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
     if (c === undefined) {
       this.print();
