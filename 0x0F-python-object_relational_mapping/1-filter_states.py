35a6db41b3d9430225d8b29663914f6b607415b2
PauliCrespi <4377@holberton.com>
2022-08-04 09:56:45 -0700

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+try:
+    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+except Exception:
+    print("ERROR")
