2b5a13f16c1521ad10cdb1f8a77c2e2b0a5003f4
PauliCrespi <4377@holberton.com>
2022-08-05 07:12:36 -0700

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    try:
+        first = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(first.id, first.name))
+        session.commit()
+    except SQLAlchemyError as e:
+        print("")
+    finally:
+        session.close()
