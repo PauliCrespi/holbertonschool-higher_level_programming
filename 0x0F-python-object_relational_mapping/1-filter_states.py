e4a00930de3dc0c569bc37c2ee3471408a23cef0
PauliCrespi <4377@holberton.com>
2022-08-06 09:41:58 -0700

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ try:
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1][0] == "N":
+            print(row)
     conn.close()
 
 except Exception:
