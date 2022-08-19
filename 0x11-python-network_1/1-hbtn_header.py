738019b18d7dcbd3d2ed3f2ce46e7ed2b6e6bf17
root <4377@holbertonschool.com>
2022-08-19 08:34:05 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,4 +5,5 @@ from sys import argv
 
 if __name__ == "__main__":
     with urllib.request.urlopen(argv[1]) as response:
+        html = urllib.read()
         print(response.headers.get('X-Request-Id'))
