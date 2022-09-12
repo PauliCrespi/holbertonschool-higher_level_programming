3dc064468a02088a2043ba049de6f05fcfd063e5
root <4377@holbertonschool.com>
2022-09-12 14:52:23 +0000

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const axios = require('axios');
+
+axios
+  .get(String(process.argv[2]))
+  .then(res => {
+    console.log(`code: ${res.status}`);
+  })
+  .catch(error => {
+    console.error(`code: ${error.response.status}`);
+  });
